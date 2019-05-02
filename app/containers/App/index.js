39d486d07d525fb650be016fc956401236761e84
Muhammad Sulman <muhmd.sulman@gmail.com>
2019-05-02 17:07:16 +0500

    initial commit

---
+++ app/containers/App/index.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * App.js
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
+ */
+
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import HomePage from 'containers/HomePage/Loadable';
+import NotFoundPage from 'containers/NotFoundPage/Loadable';
+
+import GlobalStyle from '../../global-styles';
+
+// core components
+import Admin from '../../layouts/Admin.jsx';
+import RTL from '../../layouts/RTL.jsx';
+
+import '../../assets/css/material-dashboard-react.css?v=1.6.0';
+
+export default function App() {
+  return (
+    <div>
+      <Switch>
+        {/* <Route exact path="/" component={HomePage} /> */}
+        {/*<Route component={NotFoundPage} />*/}
+        <Route path="/admin" component={Admin} />
+        <Route path="/rtl" component={RTL} />
+        <Redirect from="/" to="/admin/dashboard" />
+      </Switch>
+      {/*<GlobalStyle />*/}
+    </div>
+  );
+}
