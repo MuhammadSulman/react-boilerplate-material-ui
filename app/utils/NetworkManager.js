c498da817e4fac55b04406bdd14ca367a7b1f133
MuhammadSulman <muhmd.sulman@gmail.com>
2019-11-16 10:26:38 +0500

    feature(axios):-> implemented axios setup

---
+++ app/utils/NetworkManager.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+class Request {
+    static defaultOptions () {
+        return {
+            baseURL: "",
+            config: { headers: { 'Accept-Encoding': 'gzip' }}
+        };
+    }
+
+    static customInstance(){
+        let instance = axios.create({...this.defaultOptions()});
+
+        instance.interceptors.response.use(function (response) {
+            return response.data;
+        }, function (error) {
+            return  Promise.reject(error.response.data);
+        });
+
+        return instance;
+    }
+
+    static get(url, options = {}){
+        return this.customInstance().get(url, { ...options });
+    }
+
+    static put(url, data, options = {}){
+        return this.customInstance().put(url, data, {...options })
+    }
+
+    static post(url, data, options = {}){
+        return this.customInstance().post(url, data, { ...options })
+    }
+
+    static delete(url, options = {}){
+        return this.customInstance().delete(url, { ...options });
+    }
+
+}
+
+export default Request;
